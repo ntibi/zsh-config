cc608e2a873f2295e2eb6d9afc71eaf54baa45b2
pimparoo <NicolasTibi@yahoo.fr>
2015-09-05 20:13:39 +0200

    dr

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 echo "checking for updates..."
 git pull
 
-if [[ ! -e ~/.zshrc ]] && [[ ! -L ~/.zshrc ]];
+if [ ! -e ~/.zshrc ] && [ ! -L ~/.zshrc ];
 then
 	echo "linking ~/.zshrc with ./.zshrc"
 	ln -s "$PWD/.zshrc" "$HOME/.zshrc"
 else
-	if [[ -L ~/.zshrc ]];
+	if [ -L ~/.zshrc ];
 	then
-		if [[ $(readlink -f ~/.zshrc) = $(echo "$PWD/.zshrc") ]];
+		if [ $(readlink -f ~/.zshrc) = $(echo "$PWD/.zshrc") ];
 		then
 			echo "new .zshrc already installed"
 		else
